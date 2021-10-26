cff84af2c89b1a7e6959780794999426429e11a9
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-10-26 19:18:33 +0700

    membuat view model dan livedata untuk autentikasi

--- app/src/main/java/org/d3ifcool/aspirin/ui/authentication/login/LoginActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/authentication/login/LoginActivity.kt
@@ -1,22 +1,45 @@
 package org.d3ifcool.aspirin.ui.authentication.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseUser
+import org.d3ifcool.aspirin.data.viewmodel.authentication.AuthViewModel
 import org.d3ifcool.aspirin.databinding.ActivityLoginBinding
+import org.d3ifcool.aspirin.ui.home.sosialmedia.SosialMediaActivity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
     private val contract = FirebaseAuthUIActivityResultContract()
     private val signInLauncher = registerForActivityResult(contract) {}
+
+    private val viewModel: AuthViewModel by lazy {
+        ViewModelProvider(this).get(AuthViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.buttonGoogleLogin.setOnClickListener { startLogin() }
+
+        viewModel.authState.observe(this, { authState(it) })
+
+    }
+
+    private fun authState(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, SosialMediaActivity::class.java))
+        } else {
+            Log.d("Google_Auth", "login gagal")
+        }
     }
 
     private fun startLogin() {
