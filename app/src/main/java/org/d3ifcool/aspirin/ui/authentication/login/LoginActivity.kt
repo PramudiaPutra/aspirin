7145cd1cc19bb5cc2a216dd98dd22d5d1bb63111
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-10-26 16:25:21 +0700

    menambah Firebase UI untuk login dengan google

--- app/src/main/java/org/d3ifcool/aspirin/ui/authentication/login/LoginActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/authentication/login/LoginActivity.kt
@@ -2,15 +2,29 @@ package org.d3ifcool.aspirin.ui.authentication.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import org.d3ifcool.aspirin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
+    private val contract = FirebaseAuthUIActivityResultContract()
+    private val signInLauncher = registerForActivityResult(contract) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonGoogleLogin.setOnClickListener { startLogin() }
+    }
+
+    private fun startLogin() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(intent)
     }
 }
