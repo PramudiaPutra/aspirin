89c26eac6286f4969e2ab59e5150272871b8a327
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-10-27 08:47:54 +0700

    Cek button kirim

--- app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/PostingStoryActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/PostingStoryActivity.kt
@@ -4,11 +4,26 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.d3ifcool.aspirin.R
+import org.d3ifcool.aspirin.databinding.ActivityPostingStoryBinding
 
 class PostingStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostingStoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posting_story)
+        binding = ActivityPostingStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnKirimKegiatan.setOnClickListener {
+            posting()
+        }
     }
+
+    private fun posting(){
+        Log.d("PostingStoryActivity", "Button kirim di klik")
+    }
+
 }
