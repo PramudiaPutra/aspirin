2cdf4c63cbf54c4afc2aa05c80285b2a8d126c1d
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-10-28 19:59:33 +0700

    menampilkan foto ke dalam recycler view

--- app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/SosialMediaAdapter.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/SosialMediaAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import org.d3ifcool.aspirin.R
 import org.d3ifcool.aspirin.data.viewmodel.sosialmedia.PostingData
 import org.d3ifcool.aspirin.databinding.ItemCardviewPostinganBinding
@@ -39,7 +40,15 @@ class SosialMediaAdapter : RecyclerView.Adapter<SosialMediaAdapter.ViewHolder>()
             tvTitlePosting.text = dataPosting.judulPosting
             tvKontenPosting.text = dataPosting.deskripsiPosting
             Glide.with(profileImage.context).load(R.drawable.aspirin_main_icon).into(profileImage)
-            Glide.with(imgPosting.context).load(R.drawable.hint_rv).into(imgPosting)
+
+            val requestOption = RequestOptions()
+                .placeholder(R.drawable.icon_image)
+                .error(R.drawable.icon_broken_image)
+
+            Glide.with(imgPosting.context)
+                .applyDefaultRequestOptions(requestOption)
+                .load(dataPosting.photoUrl)
+                .into(imgPosting)
         }
     }
 
