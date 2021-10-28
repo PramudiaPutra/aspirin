0fa8a6292caed279a451d967fb1b80e956546a04
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-10-28 18:09:12 +0700

    camera belum fix

--- app/src/main/java/org/d3ifcool/aspirin/ui/camera/PhotoPreviewActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/camera/PhotoPreviewActivity.kt
@@ -1,10 +1,12 @@
 package org.d3ifcool.aspirin.ui.camera
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.otaliastudios.cameraview.PictureResult
 import org.d3ifcool.aspirin.databinding.ActivityPhotoPreviewBinding
+import org.d3ifcool.aspirin.ui.home.sosialmedia.PostingStoryActivity
 
 class PhotoPreviewActivity : AppCompatActivity() {
 
@@ -17,6 +19,12 @@ class PhotoPreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoPreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.confirmPhotoButton.setOnClickListener {
+            startActivity(
+                Intent(this, PostingStoryActivity::class.java)
+            )
+        }
 
         val result = pictureResult ?: run {
             finish()
--- app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/PostingStoryActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/home/sosialmedia/PostingStoryActivity.kt
@@ -1,15 +1,53 @@
 package org.d3ifcool.aspirin.ui.home.sosialmedia
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3ifcool.aspirin.R
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.otaliastudios.cameraview.CameraUtils
+import com.otaliastudios.cameraview.controls.PictureFormat
+import org.d3ifcool.aspirin.databinding.ActivityPostingStoryBinding
+import org.d3ifcool.aspirin.ui.camera.PhotoPreviewActivity
+import java.io.File
 
 class PostingStoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostingStoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posting_story)
-        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFFFFF")))
+        binding = ActivityPostingStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val result = PhotoPreviewActivity.pictureResult ?: run {
+            finish()
+            return
+        }
+
+        try {
+            result.toBitmap(1000, 1000) { bitmap -> binding.imgAddPosting.setImageBitmap(bitmap) }
+        } catch (e: Exception) {
+            Toast.makeText(this, "preview error $e", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun postPhoto() {
+        val extension = when (PhotoPreviewActivity.pictureResult!!.format) {
+            PictureFormat.JPEG -> "jpg"
+            else -> throw RuntimeException("wrong format.")
+        }
+        val file = File(filesDir, "picture.$extension")
+        CameraUtils.writeToFile(PhotoPreviewActivity.pictureResult!!.data, file) {
+            if (it != null) {
+                val context = this@PostingStoryActivity
+                val uri =
+                    FileProvider.getUriForFile(context, context.packageName + ".provider", file)
+            } else {
+                Toast.makeText(
+                    this@PostingStoryActivity,
+                    "Error while writing file.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
