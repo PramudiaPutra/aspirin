6b016db866c56a823d851db91d67dc940999f8ae
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-11-27 11:09:14 +0700

    convert activity splash, login, dan register ke fragment

---
+++ app/src/main/java/org/d3ifcool/aspirin/ui/SplashFragment.kt
@@ -0,0 +1,36 @@
+package org.d3ifcool.aspirin.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import org.d3ifcool.aspirin.R
+import org.d3ifcool.aspirin.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            context?.let {
+                findNavController().navigate(R.id.action_splash_to_loginFragment)
+            }
+        }, 500)
+    }
+}
--- app/src/main/java/org/d3ifcool/aspirin/ui/setting/SettingActivity.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/setting/SettingActivity.kt
@@ -1,6 +1,5 @@
 package org.d3ifcool.aspirin.ui.setting
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
@@ -8,7 +7,6 @@ import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseUser
 import org.d3ifcool.aspirin.data.viewmodel.authentication.AuthViewModel
 import org.d3ifcool.aspirin.databinding.ActivitySettingBinding
-import org.d3ifcool.aspirin.ui.authentication.login.LoginActivity
 
 class SettingActivity : AppCompatActivity() {
 
@@ -39,6 +37,6 @@ class SettingActivity : AppCompatActivity() {
 
     private fun logOut() {
         AuthUI.getInstance().signOut(this)
-        startActivity(Intent(this, LoginActivity::class.java))
+//        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
