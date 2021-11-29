25abd5ce4ac99ea8f656944c02d8c5cb49e6b835
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-11-29 15:58:28 +0700

    Membuat recyclerview comment dengan data dummy

--- app/src/main/java/org/d3ifcool/aspirin/ui/home/comment/CommentAdapter.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/home/comment/CommentAdapter.kt
@@ -1,4 +1,40 @@
 package org.d3ifcool.aspirin.ui.home.comment
 
-class CommentAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import org.d3ifcool.aspirin.R
+import org.d3ifcool.aspirin.data.model.comment.Comment
+import org.d3ifcool.aspirin.databinding.ItemCardviewPostinganBinding
+import org.d3ifcool.aspirin.databinding.ItemCommentBinding
+
+class CommentAdapter(private val listComment: ArrayList<Comment>) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCommentBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = listComment[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount(): Int {
+        return listComment.size
+    }
+
+    inner class ViewHolder(private val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Comment) = with(binding) {
+            Glide.with(profileImageComment.context).load(data.photoUser).into(profileImageComment)
+            tvNamaUserComment.text = data.username
+            tvMessage.text = data.commentContent
+
+        }
+
+    }
 }
--- app/src/main/java/org/d3ifcool/aspirin/ui/home/comment/CommentFragment.kt
+++ app/src/main/java/org/d3ifcool/aspirin/ui/home/comment/CommentFragment.kt
@@ -5,19 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.d3ifcool.aspirin.R
+import org.d3ifcool.aspirin.data.model.comment.Comment
+import org.d3ifcool.aspirin.data.model.comment.CommentData
+import org.d3ifcool.aspirin.databinding.FragmentCommentBinding
+import org.d3ifcool.aspirin.databinding.FragmentStoryBinding
+import org.d3ifcool.aspirin.ui.home.sosialmedia.SosialMediaAdapter
 
 class CommentFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentCommentBinding
+    private var list: ArrayList<Comment> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comment, container, false)
+        binding = FragmentCommentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list.addAll(CommentData.listData)
+        with(binding.recyclerViewComment){
+            val linearLayoutManager = LinearLayoutManager(context)
+            linearLayoutManager.reverseLayout = true
+            linearLayoutManager.stackFromEnd = true
+            layoutManager = linearLayoutManager
+            setHasFixedSize(true)
+            val listCommentAdapter = CommentAdapter(list)
+            adapter = listCommentAdapter
+        }
     }
 }
