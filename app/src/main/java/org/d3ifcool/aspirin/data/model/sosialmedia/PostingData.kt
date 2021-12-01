da78bcc047f215e9638a527c6801a131368a2be4
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-12-01 13:03:55 +0700

    Parcelize Comment data class

--- app/src/main/java/org/d3ifcool/aspirin/data/model/sosialmedia/PostingData.kt
+++ app/src/main/java/org/d3ifcool/aspirin/data/model/sosialmedia/PostingData.kt
@@ -12,4 +12,5 @@ data class PostingData(
     val deskripsiPosting: String? = null,
     val tanggalPosting: String? = null,
     val photoUrl: String? = null,
+    val comments: List<Comment>
 ): Parcelable
