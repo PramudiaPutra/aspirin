6b96b3c930eab5a074af9930b71f105ca737ab79
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-12-01 14:06:37 +0700

    Membuat function getComment di ViewModel

--- app/src/main/java/org/d3ifcool/aspirin/data/viewmodel/sosialmedia/CommentViewModel.kt
+++ app/src/main/java/org/d3ifcool/aspirin/data/viewmodel/sosialmedia/CommentViewModel.kt
@@ -1,7 +1,27 @@
 package org.d3ifcool.aspirin.data.viewmodel.sosialmedia
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.d3ifcool.aspirin.data.model.comment.Comment
+import org.d3ifcool.aspirin.data.model.sosialmedia.PostingData
+import org.d3ifcool.aspirin.data.network.Repo
+import org.d3ifcool.aspirin.data.viewmodel.authentication.UserLiveData
 
 class CommentViewModel : ViewModel() {
 
+    private val repo = Repo()
+
+    fun fetchCommentData(): LiveData<MutableList<Comment>> {
+        val mutableData = MutableLiveData<MutableList<Comment>>()
+        repo.getComments().observeForever {
+            mutableData.value = it
+        }
+        return mutableData
+    }
 }
